9f00bf68d39343a913e2a9ca242ece56a389ef3e
haroon 7012 <70120821@student.uol.edu.pk>
2025-02-04 03:06:52 +0500

    user id work

--- src/Components/ScheduledPosts.js
+++ src/Components/ScheduledPosts.js
@@ -265,7 +265,7 @@ const ScheduledPosts = () => {
       if (!email) return;  // Ensure email exists before making the request
 
       try {
-        const response = await axios.post('https://smp-be-mysql.vercel.app/scheduled/fetch-scheduled-posts', {
+        const response = await axios.post('https://smp-be-mysql.vercel.app/scheduled/count', {
           email: email,
         });
         setPosts(response.data);
